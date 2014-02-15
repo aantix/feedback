200fcddc7480434d9f83ea6ca9b7b27601319ace
Jim Jones <jjones@aantix.com>
2014-02-15 14:12:30 -0800

    Only run on Saturday.

--- app/models/feedback_page.rb
+++ app/models/feedback_page.rb
@@ -6,25 +6,27 @@ class FeedbackPage < ActiveRecord::Base
   belongs_to :user
 
   def self.request_feedback
-    qualifications =  {:approval_rate => {:gt => 80}, :country => {:eql => 'US'}}
+    if Date.today.wday == 6 # Saturday
+      qualifications = {:approval_rate => {:gt => 80}, :country => {:eql => 'US'}}
 
-    puts "Requesting feedback..."
-    FeedbackPage.all.each do |p|
-      t = Turkee::TurkeeTask.create_hit(host,
-                                    "Quick feedback for webpage",
-                                    "Click on the site below and explore the site a bit.  Then answer the following questions.",
-                                    :FeedbackPage,
-                                    2,    # assignments
-                                    1.51, # reward
-                                    2,    # days
-                                    5,    # hours max duration
-                                    qualifications,
-                                    {:feedback_page_id => p.id,
-                                     :user_id          => p.user.id}, {})
+      puts "Requesting feedback..."
+      FeedbackPage.all.each do |p|
+        t = Turkee::TurkeeTask.create_hit(host,
+                                          "Quick feedback for webpage",
+                                          "Click on the site below and explore the site a bit.  Then answer the following questions.",
+                                          :FeedbackPage,
+                                          2, # assignments
+                                          1.51, # reward
+                                          2, # days
+                                          5, # hours max duration
+                                          qualifications,
+                                          {:feedback_page_id => p.id,
+                                           :user_id => p.user.id}, {})
 
-      puts t.hit_url
+        puts t.hit_url
+      end
+      puts "HITs posted."
     end
-    puts "HITs posted."
   end
 
   def self.host
